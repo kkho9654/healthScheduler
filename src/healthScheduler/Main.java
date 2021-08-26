9b0941547e19b8ff343e32ee67d84f4347e6e694
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2021-08-26 09:05:58 +0900

    menu, main

--- src/healthScheduler/Main.java
+++ src/healthScheduler/Main.java
@@ -3,27 +3,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-        System.out.println("메뉴를 선택해주세요");
-        int menu = sc.nextInt();
-        if (menu == 1) {
-        	UserInfoPage a=new UserInfoPage();
-    		a.userInfo("usertest");
-        } else if (menu == 2) {
-        	AddRoutinePage a=new AddRoutinePage();
-    		a.addRoutinePage("usertest");
-        } else if (menu == 3) {
-            Food fd = new Food();
-            fd.foodRoutine("usertest") ;
-        } else if (menu == 4) {
-
-        } else {
-
-        }
-        
-		
+	public static void main(String args[])
+	{	
+		MenuPage page = new MenuPage();
 	}
-
 }
