4a7cddb0b03eeb0e1c5ece574c6295965a224f86
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2021-08-26 09:23:45 +0900

    ss

--- src/healthScheduler/MenuPage.java
+++ src/healthScheduler/MenuPage.java
@@ -1,5 +1,39 @@
 package healthScheduler;
 
-public class MenuPage {
+import java.util.Scanner;
 
+public class MenuPage {
+	Scanner sc = new Scanner(System.in);
+	public MenuPage() {
+		StartPage sp = new StartPage();
+		while(true) {
+			System.out.println("-----------");
+			System.out.println("1. 개인정보관리 ");
+			System.out.println("2. 루틴입력 ");
+			System.out.println("3. 식단추천 ");
+			System.out.println("4. 스케줄 관리 및 성취도 평가 ");
+			System.out.println("-----------");
+			System.out.print(">>> ");
+			int menu = sc.nextInt();
+			
+			if (menu == 1) {
+	        	UserInfoPage a=new UserInfoPage();
+	    		a.userInfo("usertest");
+	        } 
+			else if (menu == 2) {
+	        	AddRoutinePage a=new AddRoutinePage();
+	    		a.addRoutinePage("usertest");
+	        } 
+			else if (menu == 3) {
+	            Food fd = new Food();
+	            fd.foodRoutine("usertest") ;
+	        } 
+			else if (menu == 4) {
+				//함수추가
+	        }
+			else {
+				System.out.println("올바르지 않은 번호를 입력했습니다. 다시입력해주세요..");
+			}
+		}
+	}
 }
--- src/healthScheduler/StartPage.java
+++ src/healthScheduler/StartPage.java
@@ -1,5 +1,42 @@
 package healthScheduler;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class StartPage {
+	Scanner sc = new Scanner(System.in);	
+	String username;
+	public StartPage() 
+	{
+		while(true) {
+		System.out.println("-----------");
+		System.out.println("1. 로그인 ");
+		System.out.println("2. 회원가입 ");
+		System.out.println("-----------");
+		System.out.print(">>> ");
+		int num = sc.nextInt();
+		if(num == 1) {
+			LoginPage lp = new LoginPage();		
+			if (lp.getIs_login()==true) {				
+				username = lp.get_username();				
+				break;
+			}
+			else {										
+				continue;
+			}
+			}
+		else if(num == 2) {
+			SignupPage sp = new SignupPage();
+			
+			}
+		else {
+			System.out.println("올바르지 않은 번호를 입력했습니다. 다시입력해주세요");
+		}
+		}
+	}
 
 }
